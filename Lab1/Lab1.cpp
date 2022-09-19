d8b8098436f00e15427aecbc88b84424952b22cc
Roman Shidlovsky <jagerlion@mail.ru>
2022-09-19 13:26:28 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,259 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef UNICODE
+#define UNICODE
+#endif
+#pragma comment(lib, "mpr.lib")
+#pragma comment(lib, "netapi32.lib")
+
+
+#include <iostream>
+#include <Windows.h>
+#include <nb30.h>
+#include <stdio.h>
+#include <winnetwk.h>
+
+
+typedef struct _ASTAT_
+{
+    ADAPTER_STATUS adapt;
+    NAME_BUFFER NameBuff[30];
+}ASTAT, *PASTAT;
+
+ASTAT Adapter;
+
+void GetMacAddress()
+{
+    NCB Ncb;
+    UCHAR uRetCode;
+    LANA_ENUM lana_enum;
+
+    memset(&Ncb, 0, sizeof(Ncb));
+    Ncb.ncb_command = NCBENUM; //the ncb_buffer member points to a buffer to be filled with a LANA_ENUM structure
+    Ncb.ncb_buffer = (UCHAR*)&lana_enum;
+    Ncb.ncb_length = sizeof(lana_enum);
+    uRetCode = Netbios(&Ncb);
+
+    for (int i = 0; i < lana_enum.length; i++) {
+        memset(&Ncb, 0, sizeof(Ncb));
+        Ncb.ncb_command = NCBRESET;
+        Ncb.ncb_lana_num = lana_enum.lana[i];
+        uRetCode = Netbios(&Ncb);
+
+        memset(&Ncb, 0, sizeof(Ncb));
+        Ncb.ncb_command = NCBASTAT;
+        Ncb.ncb_lana_num = lana_enum.lana[i];
+        strcpy((char*)Ncb.ncb_callname, "*              ");//ncb_callname : Specifies the name of the remote application
+        Ncb.ncb_buffer = (unsigned char*)&Adapter;
+        Ncb.ncb_length = sizeof(Adapter);
+        uRetCode = Netbios(&Ncb); // Resets a LAN adapter
+
+        if (uRetCode == 0) {
+            printf("LANA %2d : %02X-%02X-%02X-%02X-%02X-%02X\n",
+                lana_enum.lana[i],
+                Adapter.adapt.adapter_address[0],
+                Adapter.adapt.adapter_address[1],
+                Adapter.adapt.adapter_address[2],
+                Adapter.adapt.adapter_address[3],
+                Adapter.adapt.adapter_address[4],
+                Adapter.adapt.adapter_address[5]);
+        }       
+    }
+    printf("\n");
+}
+
+void DisplayStruct(int i, LPNETRESOURCE lpnrLocal);
+
+
+BOOL WINAPI EnumerateFunc(LPNETRESOURCE lpnr)
+{
+    DWORD dwResult, dwResultEnum;
+    HANDLE hEnum;
+    DWORD cbBuffer = 16384;     // 16K is a good size
+    DWORD cEntries = -1;        // enumerate all possible entries
+    LPNETRESOURCE lpnrLocal;    // pointer to enumerated structures
+    DWORD i;
+    //
+    // Call the WNetOpenEnum function to begin the enumeration.
+    //
+    dwResult = WNetOpenEnum(RESOURCE_GLOBALNET, // all network resources
+        RESOURCETYPE_ANY,   // all resources
+        0,  // enumerate all resources
+        lpnr,       // NULL first time the function is called
+        &hEnum);    // handle to the resource
+
+    if (dwResult != NO_ERROR) {
+        printf("WnetOpenEnum failed with error %d\n", dwResult);
+        return FALSE;
+    }
+    //
+    // Call the GlobalAlloc function to allocate resources.
+    //
+    //GPTR Combines     GMEM_FIXED and GMEM_ZEROINIT.
+    //GMEM_FIXED        Allocates fixed memory.The return value is a pointer.
+    //GMEM_ZEROINIT     Initializes memory contents to zero.
+    lpnrLocal = (LPNETRESOURCE)GlobalAlloc(GPTR, cbBuffer);
+    if (lpnrLocal == NULL) {
+        printf("WnetOpenEnum failed with error %d\n", dwResult);
+        //      NetErrorHandler(hwnd, dwResult, (LPSTR)"WNetOpenEnum");
+        return FALSE;
+    }
+
+    do {
+        //
+        // Initialize the buffer.
+        //
+        ZeroMemory(lpnrLocal, cbBuffer);
+        //
+        // Call the WNetEnumResource function to continue
+        //  the enumeration.
+        //
+        dwResultEnum = WNetEnumResource(hEnum,  // resource handle
+            &cEntries,      // defined locally as -1
+            lpnrLocal,      // LPNETRESOURCE
+            &cbBuffer);     // buffer size
+//
+// If the call succeeds, loop through the structures.
+//
+        if (dwResultEnum == NO_ERROR) {
+            for (i = 0; i < cEntries; i++) {
+                // Call an application-defined function to
+                //  display the contents of the NETRESOURCE structures.
+                //
+                DisplayStruct(i, &lpnrLocal[i]);
+
+                // If the NETRESOURCE structure represents a container resource, 
+                //  call the EnumerateFunc function recursively.
+
+                if (RESOURCEUSAGE_CONTAINER == (lpnrLocal[i].dwUsage
+                    & RESOURCEUSAGE_CONTAINER))
+                    //          if(!EnumerateFunc(hwnd, hdc, &lpnrLocal[i]))
+                    if (!EnumerateFunc(&lpnrLocal[i]))
+                        printf("EnumerateFunc returned FALSE\n");
+                //            TextOut(hdc, 10, 10, "EnumerateFunc returned FALSE.", 29);
+            }
+        }
+        // Process errors.
+        //
+        else if (dwResultEnum != ERROR_NO_MORE_ITEMS) {
+            printf("WNetEnumResource failed with error %d\n", dwResultEnum);
+
+            //      NetErrorHandler(hwnd, dwResultEnum, (LPSTR)"WNetEnumResource");
+            break;
+        }
+    }
+    //
+    // End do.
+    //
+    while (dwResultEnum != ERROR_NO_MORE_ITEMS);
+    //
+    // Call the GlobalFree function to free the memory.
+    //
+    GlobalFree((HGLOBAL)lpnrLocal);
+    //
+    // Call WNetCloseEnum to end the enumeration.
+    //
+    dwResult = WNetCloseEnum(hEnum);
+
+    if (dwResult != NO_ERROR) {
+        //
+        // Process errors.
+        //
+        printf("WNetCloseEnum failed with error %d\n", dwResult);
+        //    NetErrorHandler(hwnd, dwResult, (LPSTR)"WNetCloseEnum");
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+void DisplayStruct(int i, LPNETRESOURCE lpnrLocal)
+{
+    printf("NETRESOURCE[%d] Scope: ", i);
+    switch (lpnrLocal->dwScope) {
+    case (RESOURCE_CONNECTED):
+        printf("connected\n");
+        break;
+    case (RESOURCE_GLOBALNET):
+        printf("all resources\n");
+        break;
+    case (RESOURCE_REMEMBERED):
+        printf("remembered\n");
+        break;
+    default:
+        printf("unknown scope %d\n", lpnrLocal->dwScope);
+        break;
+    }
+
+    printf("NETRESOURCE[%d] Type: ", i);
+    switch (lpnrLocal->dwType) {
+    case (RESOURCETYPE_ANY):
+        printf("any\n");
+        break;
+    case (RESOURCETYPE_DISK):
+        printf("disk\n");
+        break;
+    case (RESOURCETYPE_PRINT):
+        printf("print\n");
+        break;
+    default:
+        printf("unknown type %d\n", lpnrLocal->dwType);
+        break;
+    }
+
+    printf("NETRESOURCE[%d] DisplayType: ", i);
+    switch (lpnrLocal->dwDisplayType) {
+    case (RESOURCEDISPLAYTYPE_GENERIC):
+        printf("generic\n");
+        break;
+    case (RESOURCEDISPLAYTYPE_DOMAIN):
+        printf("domain\n");
+        break;
+    case (RESOURCEDISPLAYTYPE_SERVER):
+        printf("server\n");
+        break;
+    case (RESOURCEDISPLAYTYPE_SHARE):
+        printf("share\n");
+        break;
+    case (RESOURCEDISPLAYTYPE_FILE):
+        printf("file\n");
+        break;
+    case (RESOURCEDISPLAYTYPE_GROUP):
+        printf("group\n");
+        break;
+    case (RESOURCEDISPLAYTYPE_NETWORK):
+        printf("network\n");
+        break;
+    default:
+        printf("unknown display type %d\n", lpnrLocal->dwDisplayType);
+        break;
+    }
+
+    printf("NETRESOURCE[%d] Usage: 0x%x = ", i, lpnrLocal->dwUsage);
+    if (lpnrLocal->dwUsage & RESOURCEUSAGE_CONNECTABLE)
+        printf("connectable ");
+    if (lpnrLocal->dwUsage & RESOURCEUSAGE_CONTAINER)
+        printf("container ");
+    printf("\n");
+
+    printf("NETRESOURCE[%d] Localname: %S\n", i, lpnrLocal->lpLocalName);
+    printf("NETRESOURCE[%d] Remotename: %S\n", i, lpnrLocal->lpRemoteName);
+    printf("NETRESOURCE[%d] Comment: %S\n", i, lpnrLocal->lpComment);
+    printf("NETRESOURCE[%d] Provider: %S\n", i, lpnrLocal->lpProvider);
+    printf("\n");
+}
+
+int main()
+{
+    printf("MAC addresses:\n");
+    GetMacAddress();
+
+    LPNETRESOURCE lpnr = NULL;
+
+    if (EnumerateFunc(lpnr) == FALSE) {
+        printf("Call to EnumerateFunc failed\n");
+        return 1;
+    }
+    else
+        return 0;
+}
